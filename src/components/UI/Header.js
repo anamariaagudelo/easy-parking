f3b1dd122b8855f3b284d21ef504b560ba43763a
Ana Maria Agudelo <anaagudelo@Anas-MacBook-Pro.local>
2019-09-07 23:18:26 -0500

    some form validations

--- src/components/UI/Header.js
+++ src/components/UI/Header.js
@@ -1,19 +1,26 @@
 import React from 'react';
-import { Navbar, NavbarBrand } from 'reactstrap';
+import { Navbar, NavbarBrand, Col, Row } from 'reactstrap';
 import logo from '../../assets/img/logo.png';
 
 
-class Header extends React.Component{
+class Header extends React.Component {
 
-    render() {
-        return (
-        <Navbar color="dark" className="navbar-dark navbar-expand-sm" toggleable>
-           <NavbarBrand href="/" className="mar-auto">
-                    <img src={logo} className="Header-logo" alt="logo" />
-            </NavbarBrand>
-        </Navbar>
-        );
-      }
-    }
+  render() {
+    return (
+      <Navbar color="dark" className="navbar-dark navbar-expand-sm" toggleable>
+        <NavbarBrand href="/" className="mar-auto">
+          <Row className="mt-2">
+            <Col md="3">
+              <img src={logo} className="Header-logo" alt="logo" />
+            </Col>
+            <Col md="9">
+              <h1>Easy Parking</h1>
+            </Col>
+          </Row>
+        </NavbarBrand>
+      </Navbar>
+    );
+  }
+}
 
 export default Header;
